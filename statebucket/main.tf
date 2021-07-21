fea0cf34575cc4a38a0b0a9673a485152e298bca
Antonio Rufo <antcuenco@gmail.com>
2021-07-21 21:04:16 +1000

    Added statebucket

---
+++ statebucket/main.tf
@@ -0,0 +1,54 @@
+###############################################################################
+# Providers
+###############################################################################
+provider "aws" {
+  region              = var.region
+  allowed_account_ids = [var.aws_account_id]
+}
+
+###############################################################################
+# Terraform main config
+# terraform block cannot be interpolated; sample provided as output of _main
+# `terraform output remote_state_configuration_example`
+###############################################################################
+terraform {
+  required_version = ">= 1.0.2"
+  required_providers {
+    aws = {
+      version = "~> 3.41.0"
+    }
+  }
+}
+
+###############################################################################
+# Data Sources and Locals
+###############################################################################
+data "aws_caller_identity" "current" {}
+
+# Remote State Locals
+locals {
+  tags = {
+    Environment = var.environment
+  }
+}
+###############################################################################
+# S3 Bucket for Terraform state
+###############################################################################
+resource "aws_s3_bucket" "state" {
+  bucket        = "${data.aws_caller_identity.current.account_id}-build-state-bucket-cicd-setup"
+  force_destroy = true
+
+  tags = local.tags
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
---
+++ statebucket/terraform.tfvars
@@ -0,0 +1,6 @@
+###############################################################################
+# Environment
+###############################################################################
+aws_account_id = "130541009828"   ### PLEASE UPDATE THE AWS ACCOUNT NUMBER
+environment    = "Development"    ### PLEASE UPDATE YOUR ENVIRONMENT IF NEEDED
+region         = "ap-southeast-2" ### PLEASE UPDATE THE AWS REGION
